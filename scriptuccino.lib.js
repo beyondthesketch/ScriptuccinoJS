08286b5763d16e6bb2f286687dbbe8cee1b08163
NajuDev <NajuDev@users.noreply.github.com>
2018-11-09 23:02:27 +0000

    add webpack, change babel config, prepare file for es5 version

---
+++ scriptuccino.lib.js
@@ -0,0 +1,6 @@
+import * as events from './src/events/index.js';
+
+self.SCRIPTUCCINO = self.SCRIPTUCCINO || {};
+Object.assign(self.SCRIPTUCCINO, {
+   events, 
+});
---
+++ webpack.prod.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './scriptuccino.lib.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'scriptuccino.lib.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [path.resolve(__dirname, 'src/events')],
+                exclude: [path.resolve(__dirname, 'node_modules')],
+                loader: 'babel-loader',
+                options: {presets: ['@babel/preset-env']}
+            }
+        ]
+    }
+};
