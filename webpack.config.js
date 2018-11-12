0f721ad4ba6f160f46b7064a511fcf6acd2cc405
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2018-11-12 21:22:30 +0000

    Revert "correct webpack config"
    This reverts commit 0399dc78a7d5e476959249e2df581d1b9f0b4d8c.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                include: [path.resolve(__dirname, 'src')],
+                include: [path.resolve(__dirname, 'src/events')],
                 exclude: [path.resolve(__dirname, 'node_modules')],
                 loader: 'babel-loader',
                 options: {presets: ['@babel/preset-env']}
