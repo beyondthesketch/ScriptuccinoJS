d2451efd9c93ce312b9f4e22b52fd0fe6b4f7a8a
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2018-11-13 23:10:46 +0000

    WIP - add XHR util and test

---
+++ src/utils/XHR.js
@@ -0,0 +1,207 @@
+const XHR = (config) => {
+
+    /*
+    config object design
+
+    {
+        uri             : {string},		// url of file or api
+        method          : {string},		// 'GET' or 'POST',
+        data            : {object},     // used with POST requests to submit data to the API/server
+        cached          : {boolean}, 	// Allow a cached response - only used for GET requests
+        completeFn      : {function},	// callback function run on complete - whether successful or not
+        successFn       : {function},	// callback function to run on successful response
+        errorFn         : {function},	// callback function to run when an error/failure occurs
+        openedFn        : {function},	// callback function to run when the request is opened
+        loadingFn       : {function},	// callback function to run when the request enters the loading state
+        abortFn         : {function},	// callback function to run when the request is aborted
+        timeoutFn       : {function},	// callback function to run if the request times out
+        endFn           : {function},   // callback function to run when the loadend event is fired
+        timeoutLimit    : {number},		// an integer of milliseconds to wait before time out
+        responseType    : {string},		// a string of the desired response type
+        setHeaders      : {object}		// an object of headers to be set on the XHR
+        send            : {boolean} // default true - send the request immediately
+        withCredentials : {boolean}
+    }
+    */
+    if (typeof config === 'object' && config.uri) {
+        const uri =
+          config.cached === true
+          && config.method === 'GET'
+            ?
+            config.uri + '?c=' + (new Date()).getTime()
+            :
+            config.uri;
+
+        const data_request = new XMLHttpRequest();
+
+        if ( config.responseType ) {
+          data_request.responseType = config.responseType.toLowerCase();
+        }
+
+        data_request.timeout =
+          config.timeoutLimit
+          && typeof config.timeoutLimit === 'number'
+            ?
+            config.timeoutLimit
+            :
+            10000;
+
+        data_request.open((config.method || 'GET'), uri, true);
+
+        if (
+          !('onloadstart' in data_request)
+          && !('onprogress' in data_request)
+          && !('onload' in data_request)
+          && !('onerror' in data_request)
+        ) {
+          console.log('using ready state');
+          // does not support event methods - use readystatechange instead
+          data_request.onreadystatechange = () => {
+            switch (data_request.readyState) {
+              case 1:
+                config.openedFn
+                && typeof config.openedFn === 'function'
+                && config.openedFn();
+                break;
+              case 2:
+                config.loadingFn
+                && typeof config.loadingFn === 'function'
+                && config.loadingFn();
+                break;
+              case 3:
+                config.completeFn
+                && typeof config.completeFn === 'function'
+                && config.completeFn();
+                break;
+              case 4:
+                if (data_request.status === 200) {
+                  config.successFn
+                  && typeof config.successFn === 'function'
+                  && config.successFn();
+                }
+                else if (data_request.status !== 200) {
+                  config.errorFn
+                  && typeof config.errorFn === 'function'
+                  config.errorFn();
+                }
+                break;
+            }
+          };
+        }
+        else {
+          console.log('using event methods');
+          if (
+            'onloadstart' in data_request
+            && config.openedFn
+            && typeof config.openedFn === 'function'
+          ) {
+              data_request.onloadstart = config.openedFn();
+          }
+
+          // attach loading/progress callback
+          if ('onprogress' in data_request && config.loadingFn
+            && typeof config.loadingFn === 'function'
+          ) {
+              config.loadingFn();
+          }
+
+          // attach success and complete callbacks
+          if ('onload' in data_request
+            && (
+              (config.successFn
+              && typeof config.completeFn === 'function')
+              ||
+              (config.completeFn
+              && typeof config.completeFn === 'function')
+            )
+          ) {
+              if (data_request.status === 200) {}
+              data_request.onload = () => {
+                if (config.successFn) {
+                  config.successFn(
+                    'response' in data_request
+                      ?
+                      data_request.response
+                      :
+                      responseType === 'text'
+                        ?
+                        data_request.responseText
+                        : data_request.responseXML
+
+                  );
+                }
+                if (config.completeFn) {
+                  config.completeFn();
+                }
+              };
+          }
+
+          // attach timeout callback
+          if ('ontimeout' in data_request) {
+            config.timeoutFn && typeof config.timeoutFn === 'function'
+              ?
+              data_request.ontimeout = config.timeoutFn
+              : () => console && console.error(
+                      'xhr: operation timed out - the server did not respond in time'
+                    );
+          }
+
+          // attach error callback
+          if (
+            'onerror' in data_request && config.errorFn
+            && typeof config.errorFn === 'function'
+          ) {
+            data_request.onerror = config.errorFn;
+          }
+        }
+
+        // attach abort function
+        if (
+          'onabort' in data_request && config.abortFn
+          && typeof config.abortFn === 'function'
+        ) {
+            data_request.onabort = config.abortFn;
+        }
+
+        // attach loadend callback
+        if (
+          'onloadend' in data_request && config.endFn
+          && typeof config.endFn === 'function'
+        ) {
+            data_request.onloadend = config.endFn;
+        }
+
+        // set headers
+        // set request header
+        if (config.setHeaders && typeof config.setHeaders === 'object') {
+          for (let i in config.setHeaders) {
+            if (config.setHeaders.hasOwnProperty(i)) {
+              data_request.setRequestHeader(i, config.setHeaders[i] );
+            }
+          }
+        }
+
+        if (config.withCredentials) {
+          data_request.withCredentials = true;
+        }
+
+        if (!('send' in config) || config.send) {
+          data_request.send(
+            (
+              config.method === 'POST'
+              || config.method === 'PATCH'
+            )
+            && config.data
+            && typeof config.data === 'object'
+            ?
+            config.data
+            :
+            null
+          );
+        }
+
+        return data_request;
+    }
+
+};
+export default XHR;
---
+++ tests/utils/XHR.test.js
@@ -0,0 +1,114 @@
+import XHR from './../../src/utils/XHR.js';
+
+const mockXHR = jest.fn(() => (
+  {
+    open: jest.fn(),
+    send: jest.fn(),
+    onreadystatechange: jest.fn()
+  }
+));
+
+const originalXHR = Object.assign(Object.create(null), global.XMLHttpRequest);
+
+// beforeEach(() => {
+//   global.XMLHttpRequest = mockXHR;
+// });
+// afterEach(() => {
+//   Object.defineProperty(global, 'XMLHttpRequest', {
+//     value: originalXHR
+//   });
+// });
+
+test('Returns instance of XHR', () => {
+  const request = XHR({
+    uri: '/foo',
+    send: false
+  });
+
+  expect(request).toBeInstanceOf(global.XMLHttpRequest);
+});
+
+test('Returns undefined if no config is provided', () => {
+  const request = XHR();
+
+  expect(request).toBeUndefined();
+});
+
+test('Returns undefined if provided config object does not have a URI', () => {
+  const request = XHR({
+    method: 'GET'
+  });
+
+  expect(request).toBeUndefined();
+});
+
+test('Sets the responseType if provided', () => {
+  global.XMLHttpRequest = mockXHR;
+  const request = XHR({
+    uri: '/foo',
+    responseType: 'json'
+  });
+
+  expect(request.responseType).toBe('json');
+
+  global.XMLHttpRequest = originalXHR;
+});
+
+test('Sets the responseType if provided', () => {
+  global.XMLHttpRequest = mockXHR;
+  const request = XHR({
+    uri: '/foo',
+    send: false,
+    responseType: 'json'
+  });
+
+  expect(request.responseType).toBe('json');
+
+  global.XMLHttpRequest = originalXHR;
+});
+
+test('Sets the timeout to the supplied value if provided', () => {
+  global.XMLHttpRequest = mockXHR;
+  const request = XHR({
+    uri: '/foo',
+    send: false,
+    timeoutLimit: 1500
+  });
+
+  expect(request.timeout).toBe(1500);
+
+  global.XMLHttpRequest = originalXHR;
+});
+
+test('Sets the timeout to the 10s if no value provided', () => {
+  global.XMLHttpRequest = mockXHR;
+  const request = XHR({
+    uri: '/foo',
+    send: false,
+  });
+
+  expect(request.timeout).toBe(10000);
+
+  global.XMLHttpRequest = originalXHR;
+});
+
+test('Calls open with GET if no method supplied', () => {
+  global.XMLHttpRequest = mockXHR;
+  const request = XHR({
+    uri: '/foo',
+    send: false
+  });
+
+  expect(request.open).toHaveBeenCalledWith('GET', '/foo', true);
+});
+
+test('Calls open with supplied method', () => {
+  global.XMLHttpRequest = mockXHR;
+  const request = XHR({
+    uri: '/foo',
+    send: false,
+    method: 'POST'
+  });
+
+  expect(request.open).toHaveBeenCalledWith('POST', '/foo', true);
+});
