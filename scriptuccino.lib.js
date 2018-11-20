c43f0ffae96f27c0460605ca540ca876391123f1
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2018-11-20 21:56:36 +0000

    Add utils modules to es5 version

--- scriptuccino.lib.js
+++ scriptuccino.lib.js
@@ -1,6 +1,8 @@
 import * as events from './src/events/index.js';
+import * as utils from './src/utils/index.js';
 
 self.SCRIPTUCCINO = self.SCRIPTUCCINO || {};
 Object.assign(self.SCRIPTUCCINO, {
-   events, 
+   events,
+   utils
 });
