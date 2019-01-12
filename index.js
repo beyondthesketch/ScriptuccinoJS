add28f6b0b84c5d04c48fcf7120f7cde431c5508
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2019-01-12 09:45:01 +0000

    Add parallelLoadCSS to exports

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 export {whenPageLoaded, whenPageReady} from './modules/events/index.js';
-export {XHR, lazyLoadCSS} from './modules/utils/index.js';
+export {XHR, lazyLoadCSS, parallelLoadCSS} from './modules/utils/index.js';
