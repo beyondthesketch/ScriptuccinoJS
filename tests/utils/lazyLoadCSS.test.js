d88700ce23d3620bf580e2a9da6328819dea7898
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2019-01-14 21:20:00 +0000

    Unit tests for lazyLoadCSS and parallelLoadCSS

---
+++ tests/utils/lazyLoadCSS.test.js
@@ -0,0 +1,45 @@
+jest.mock('./../../src/events/page/whenPageLoaded.js', () => jest.fn());
+import whenPageLoaded from './../../src/events/page/whenPageLoaded';
+
+jest.mock('./../../src/utils/XHR.js', () => jest.fn());
+import XHR from './../../src/utils/XHR';
+
+import lazyLoadCSS from './../../src/utils/lazyLoadCSS.js';
+
+const originalDocumentCreateElement = global.document.createElement;
+
+afterEach(() => {
+  global.document.createElement = originalDocumentCreateElement;
+  jest.clearAllMocks();
+});
+
+test('Calls document.createElement with \'style\'', () => {
+  global.document.createElement = jest.fn().mockReturnValue({
+    setAttribute: jest.fn()
+  });
+  lazyLoadCSS('/foo.css');
+
+  expect(global.document.createElement).toHaveBeenCalledWith('style');
+});
+
+
+test('Calls setAttribute', () => {
+  const mockElementInstance = {
+    setAttribute: jest.fn()
+  };
+  global.document.createElement = jest.fn().mockReturnValue(
+    mockElementInstance
+  );
+  lazyLoadCSS('/foo.css');
+
+  expect(mockElementInstance.setAttribute).toHaveBeenCalledWith(
+    'rel',
+    'stylesheet'
+  );
+});
+
+test('Calls whenPageLoaded', () => {
+  lazyLoadCSS('/public/data/foo.css');
+
+  expect(whenPageLoaded).toHaveBeenCalled();
+});
---
+++ tests/utils/parallelLoadCSS.test.js
@@ -0,0 +1,45 @@
+jest.mock('./../../src/events/page/whenPageReady.js', () => jest.fn());
+import whenPageReady from './../../src/events/page/whenPageReady';
+
+jest.mock('./../../src/utils/XHR.js', () => jest.fn());
+import XHR from './../../src/utils/XHR';
+
+import parallelLoadCSS from './../../src/utils/parallelLoadCSS.js';
+
+const originalDocumentCreateElement = global.document.createElement;
+
+afterEach(() => {
+  global.document.createElement = originalDocumentCreateElement;
+  jest.clearAllMocks();
+});
+
+test('Calls document.createElement with \'style\'', () => {
+  global.document.createElement = jest.fn().mockReturnValue({
+    setAttribute: jest.fn()
+  });
+  parallelLoadCSS('/bar.css');
+
+  expect(global.document.createElement).toHaveBeenCalledWith('style');
+});
+
+
+test('Calls setAttribute', () => {
+  const mockElementInstance = {
+    setAttribute: jest.fn()
+  };
+  global.document.createElement = jest.fn().mockReturnValue(
+    mockElementInstance
+  );
+  parallelLoadCSS('/bar.css');
+
+  expect(mockElementInstance.setAttribute).toHaveBeenCalledWith(
+    'rel',
+    'stylesheet'
+  );
+});
+
+test('Calls whenPageReady', () => {
+  parallelLoadCSS('/bar.css');
+
+  expect(whenPageReady).toHaveBeenCalled();
+});
