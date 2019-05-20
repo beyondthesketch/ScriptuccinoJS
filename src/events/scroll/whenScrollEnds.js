18313506ef439ce40f2f15c5cd5aef7b47afb66f
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2019-05-20 16:13:26 +0100

    WIP - whenScrollEnds refactor

--- src/events/scroll/whenScrollEnds.js
+++ src/events/scroll/whenScrollEnds.js
@@ -1,59 +1,42 @@
-/** ScriptuccinoJS - whenPageReady | Copyright (c) Beyond The Sketch Ltd | Licensed under MIT License */
+/** ScriptuccinoJS - whenScrollEnds | Copyright (c) Beyond The Sketch Ltd | Licensed under MIT License */
 
-const  whenScrollEnds = (
-    function () {
-        self.SCRIPTUCCINO = self.SCRIPTUCCINO || {};
-        const ns = self.SCRIPTUCCINO;
-        const scrollend_queue = ns.scrollend_queue || (
-            Object.defineProperty(ns, 'scrollend_queue', {
-                value: []
-            }),
-            ns.scrollend_queue
+function debounce(fn, wait) {
+    let t;
+    // return a function
+    return function() {
+        self.clearTimeout(t);
+        t = self.setTimeout(fn.bind(this, arguments), wait);
+    }
+}
+
+const whenScrollEnds = (
+    function() {
+        const queue = [];
+
+        self.addEventListener(
+            'scroll',
+            debounce(
+                function () {
+                    console.log('debounced!');
+                    if (!!queue.length) {
+                        queue.forEach(
+                            (queuedFn) => queuedFn()
+                        );
+                    }
+                },
+                250
+            ),
+            false
         );
 
-        window.addEventListener('scroll', () => { 
-
-                // debounce the call!
-                if (!!scrollend_queue.length) {
-
-                    // get scroll position
-
-                    // calculate viewport start and end - not needed?
-
-                    // check queue for anything inside that range
-                    ns.scrollend_queue.forEach(
-                        (elm) => {
-                            if (!elm.done && elm.position < 1) {
-                                // fire any functions that match
-                                elm.fn();
-                                elm.done = true;
-                            }
-                        }
-                    )
-
-                }
-        }, false);
-
-        return (element, fn) => {
-            // reject non html element
-            if (!element instanceof window.HTMLElement) {
-                console.warn('whenScrollEnds not supplied with a DOM element');
-                return null;
+        return (fn) => {
+            if (fn instanceof self.Function) {
+                queue.push(fn);
             }
-            // figure out position in DOM
-
-            
-
-            // push the function and element pos as an object to the queue
-            ns.scrollend_queue.push({
-                element,
-                done: false,
-                fn,
-                get position() {
-                   this.element.getBoundingClientRect().top - window.innerHeight;
-                }
-            });
-        }
+            else {
+                console && console.warn('whenScrollEnds not supplied a function');
+            }
+        };
 
     }()
 );
