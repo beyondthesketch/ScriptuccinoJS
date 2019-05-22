e25ce057ead89746b8f0e5efb8d2575c23037c93
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2019-05-22 11:18:22 +0100

    Add test for whenScrollEnds

--- tests/events/scroll/whenScrollEnds.test.js
+++ tests/events/scroll/whenScrollEnds.test.js
@@ -1,12 +1,47 @@
 import whenScrollEnds from './../../../src/events/scroll/whenScrollEnds';
 
-const originalDocument = Object.assign({}, global.document);
+const addEventListenerSpy = jest.spyOn(window, 'addEventListener');
+const consoleWarnSpy = jest.spyOn(global.console, 'warn');
+const arrayPushSpy = jest.spyOn(Array.prototype, 'push');
 
-beforeEach(() => {
-    global.SCRIPTUCCINO.scrollend_queue.splice(0);
+afterAll(() => {
+    addEventListenerSpy.mockRestore();
+    consoleWarnSpy.mockRestore();
+    arrayPushSpy.mockRestore();
 });
-afterEach(() => {
-    Object.defineProperty(global, 'document', {
-        value: originalDocument,
-    });
+
+test('Calls addEventListener on first run', () => {
+    whenScrollEnds(() => null);
+
+    expect(addEventListenerSpy).toHaveBeenCalled();
+
+});
+
+test('Calls addEventListener on first run with the expected arguments', () => {
+    whenScrollEnds(() => null);
+
+    expect(addEventListenerSpy.mock.calls[0][0]).toBe('scroll');
+    expect(addEventListenerSpy.mock.calls[0][1]).toBeInstanceOf(Function);
+    expect(addEventListenerSpy.mock.calls[0][2]).toBe(false);
+});
+
+test('Does not call addEventListener on subsequent runs', () => {
+    whenScrollEnds(() => null);
+    whenScrollEnds(() => null);
+    whenScrollEnds(() => null);
+
+    expect(addEventListenerSpy).toHaveBeenCalledTimes(1);
+});
+
+test('Calls console.warn if no function is supplied', () => {
+    whenScrollEnds('foo');
+
+    expect(consoleWarnSpy).toHaveBeenCalledWith('whenScrollEnds not supplied a function');
+});
+
+test('Calls array.push with the supplied function', () => {
+    const mockFunction = () => null;
+    whenScrollEnds(mockFunction);
+
+    expect(arrayPushSpy).toHaveBeenCalledWith((mockFunction));
 });
