22fd0857571590d92ab130882e67fe1ce8eab434
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2019-08-22 06:55:17 +0100

    fix: Add missing whenScrollEnds and elementScrolledAboveFold exports

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 export {default as whenPageLoaded} from './modules/events/page/whenPageLoaded.js';
 export {default as whenPageReady} from './modules/events/page/whenPageReady.js';
+export {default as whenScrollEnds} from './modules/events/scroll/whenScrollEnds.js';
 export {default as XHR} from './modules/utils/XHR.js';
 export {default as lazyLoadCSS} from './modules/utils/lazyLoadCSS.js';
 export {default as parallelLoadCSS} from './modules/utils/parallelLoadCSS.js';
 export {default as applyTransition} from './modules/fx/applyTransition.js';
+export {default as elementScrolledAboveFold} from './modules/utils/elementScrolledAboveFold.js';
 export {default as fadeTo} from './modules/fx/fadeTo.js';
 export {default as fadeIn} from './modules/fx/fadeIn.js';
 export {default as fadeOut} from './modules/fx/fadeOut.js';
