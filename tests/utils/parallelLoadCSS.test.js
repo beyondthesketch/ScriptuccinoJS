5105e714599362103c70fbb4bcb3a132619ee1f4
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2019-10-23 22:23:01 +0100

    fix: issue #5 - More reliable mime type check instead of file extension checking for lazyLoadCSS and parallelLoadCSS and remove XHR util dependency

--- tests/utils/parallelLoadCSS.test.js
+++ tests/utils/parallelLoadCSS.test.js
@@ -1,42 +1,7 @@
 jest.mock('./../../src/events/page/whenPageReady.js', () => jest.fn());
 import whenPageReady from './../../src/events/page/whenPageReady';
 
-jest.mock('./../../src/utils/XHR.js', () => jest.fn());
-import XHR from './../../src/utils/XHR';
-
 import parallelLoadCSS from './../../src/utils/parallelLoadCSS.js';
-
-const originalDocumentCreateElement = global.document.createElement;
-
-afterEach(() => {
-  global.document.createElement = originalDocumentCreateElement;
-  jest.clearAllMocks();
-});
-
-test('Calls document.createElement with \'style\'', () => {
-  global.document.createElement = jest.fn().mockReturnValue({
-    setAttribute: jest.fn()
-  });
-  parallelLoadCSS('/bar.css');
-
-  expect(global.document.createElement).toHaveBeenCalledWith('style');
-});
-
-
-test('Calls setAttribute', () => {
-  const mockElementInstance = {
-    setAttribute: jest.fn()
-  };
-  global.document.createElement = jest.fn().mockReturnValue(
-    mockElementInstance
-  );
-  parallelLoadCSS('/bar.css');
-
-  expect(mockElementInstance.setAttribute).not.toHaveBeenCalledWith(
-    'rel',
-    'stylesheet'
-  );
-});
 
 test('Calls whenPageReady', () => {
   parallelLoadCSS('/bar.css');
